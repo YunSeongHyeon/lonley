c3c1698e12275dc1ff2297db40b478d582fc860f
sktkstka <sktkstka@gmail.com>
2016-11-10 11:28:30 +0900

    ddd
    ddd

--- src/example/lololololol.java
+++ src/example/lololololol.java
@@ -1,7 +1,7 @@
 package example;
 
 public class lololololol {
-public static void main(String[] args) {
-	System.out.println("좀!");
-}
+	public static void main(String[] args) {
+		System.out.println("좀!");
+	}
 }
